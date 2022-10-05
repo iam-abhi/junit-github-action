6a3e5004823bfc95b0ffc5a6fec703f6df1c41b6
iamabhi <abhishekverma@acciojob.com>
2022-10-05 12:25:10 +0530

    action updated

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as github from '@actions/github';
-import * as child from 'child_process';
 import fs from 'fs';
 import path from 'path';
 
@@ -15,24 +14,24 @@ import path from 'path';
 */
 
 async function run(): Promise<void> {
+  const ACCIO_API_ENDPOINT =
+      'https://accio-release-1-dot-acciojob-prod.el.r.appspot.com';
+  const githubRepo = process.env['GITHUB_REPOSITORY'];
   const repoWorkSpace: string | undefined = process.env['GITHUB_WORKSPACE'];
+  let studentUserName = '';
+  let assignmentName = '';
+  let token
   try {
     process.stderr.write(`\n1111`)
-    const githubRepo = process.env['GITHUB_REPOSITORY'];
     if (!githubRepo) throw new Error('No GITHUB_REPOSITORY');
 
     const [repoOwner, repoName] = githubRepo.split('/');
-    // const token = process.env['ACCIO_ASGMNT_ACTION_TOKEN'];
-    const ACCIO_API_ENDPOINT =
-      'https://accio-release-1-dot-acciojob-prod.el.r.appspot.com';
+    token = '1E46AD26F9A4EE2C3C8F927566721'
 
-    // if (!token) throw new Error('No token given!');
+    if (!token) throw new Error('No token given!');
     if (!repoWorkSpace) throw new Error('No GITHUB_WORKSPACE');
-    // if (repoOwner !== 'acciojob') throw new Error('Error not under acciojob');
+    if (repoOwner !== 'acciojob') throw new Error('Error not under acciojob');
     if (!repoName) throw new Error('Failed to parse repoName');
-
-    let studentUserName = '';
-    let assignmentName = '';
 
     const contextPayload = github.context.payload;
     process.stderr.write(`\n${githubRepo}`)
@@ -63,79 +62,134 @@ async function run(): Promise<void> {
     process.stdout.write(
       `Pusher Username = ${contextPayload.pusher.username}\nPusher Name = ${contextPayload.pusher.name}`
     );
+
     process.stderr.write(`\n2222`)
-
+    process.stderr.write(`\n${assignmentName}`)
+    process.stderr.write(`\n${studentUserName}`)
+    
     if (true) {
-      // const accioTestConfigData = fs.readFileSync(
-      //   path.resolve(repoWorkSpace, 'acciotest.json')
-      // );
-      // const accioTestConfig = JSON.parse(accioTestConfigData.toString());
+      const accioTestConfigData = fs.readFileSync(
+        path.resolve(repoWorkSpace, 'acciotest.json')
+      );
+      
+      const accioTestConfig = JSON.parse(accioTestConfigData.toString());
 
-      // process.stdout.write(`Test Config: ${accioTestConfigData.toString()}`);
-
-      // const query = new URLSearchParams();
-      // query.append('repo', accioTestConfig.testRepo);
-      // query.append('filePath', accioTestConfig.pathToFile);
-      // query.append('token', token);
+      const query = new URLSearchParams();
+      query.append('repo', accioTestConfig.testRepo);
+      query.append('filePath', accioTestConfig.pathToFile);
+      query.append('token', token);
 
       // Get the encoded test file contents
-      // const encodedTestFileData = await axios.get(
-      //   `${ACCIO_API_ENDPOINT}/github/action-get-file?${query.toString()}`
-      // );
+      const encodedTestFileData = await axios.get(
+        `${ACCIO_API_ENDPOINT}/github/action-get-file?${query.toString()}`
+      );
 
-      // const testFileContent = Buffer.from(
-      //   encodedTestFileData.data,
-      //   'base64'
-      // ).toString('utf8');
+      const testFileContent = Buffer.from(
+        encodedTestFileData.data,
+        'base64'
+      ).toString('utf8');
 
-      // fs.mkdirSync(path.resolve(repoWorkSpace, 'cypress/integration/tests'), {
-      //   recursive: true
-      // });
+      fs.mkdirSync(path.resolve(repoWorkSpace, 'src/test/java/com/driver/test'), {
+        recursive: true
+      });
 
-      // fs.writeFileSync(
-      //   path.resolve(repoWorkSpace, 'cypress/integration/tests/test.spec.js'),
-      //   testFileContent
-      // );
+      fs.writeFileSync(
+        path.resolve(repoWorkSpace, 'src/test/java/com/driver/test/TestCases.java'),
+        testFileContent
+      );
 
       const mvnInstall = await exec.exec('mvn install', undefined, {
         cwd: repoWorkSpace
       });
-      process.stderr.write(`\n3333`)
-      process.stderr.write(`\n${mvnInstall}`)
+
       const junitReports = fs.readFileSync(
-        path.resolve(repoWorkSpace, 'target/surefire-reports/com.driver.test.VehicleTest.txt')
+        path.resolve(repoWorkSpace, 'target/surefire-reports/com.driver.test.TestCases.txt')
       );
       let junitString = junitReports.toString();
       junitString = junitString.split('\n')[3];
       process.stderr.write(`\n${junitString}`);
-      let testResults = junitString.replace(/[^0-9.]/g,' ').split(' ');
-      testResults = testResults.filter(element => !['.',''].includes(element));
-      process.stderr.write(`\nTotal Test Cases: ${parseInt(testResults[0])}`);
-      process.stderr.write(`\nFailed Test Cases: ${parseInt(testResults[1])}`);
+      let testResult = junitString.replace(/[^0-9.]/g,' ').split(' ');
+      testResult = testResult.filter(element => !['.',''].includes(element));
+      
+      process.stdout.write(`\nTotal Test Cases: ${parseInt(testResult[0])}`);
+      process.stdout.write(`\nFailed Test Cases: ${parseInt(testResult[1])}`);
 
       process.stdout.write(`\nEvaluating score...\n`);
+      
+      const totalTests = parseInt(testResult[0]);
+      const totalPassed = (parseInt(testResult[0]) - parseInt(testResult[1]));
+
+      let testResults = {
+        totalTests,
+        totalPassed,
+      }
+      
+      process.stdout.write(`\n${token}`);
+      process.stdout.write(`\n${testResults}`);
+      process.stdout.write(`\n${assignmentName}`);
+      process.stdout.write(`\n${repoName}`);
+      process.stdout.write(`\n${studentUserName}`);
+
+      const {data: score} = await axios.post(
+        `${ACCIO_API_ENDPOINT}/github/get-score`,
+        {
+          token,
+          testResults,
+          assignmentName,
+          repoName,
+          studentGithubUserName: studentUserName
+        }
+      );
 
       process.exit(0);
     }
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message);
-    process.stderr.write(`\nError: ${(error as Error).message}`);
-    process.exit(1);
-  } finally {
-    if(repoWorkSpace){
+    if(repoWorkSpace && githubRepo){
+      const [repoOwner, repoName] = githubRepo.split('/');
+
       const junitReports = fs.readFileSync(
-        path.resolve(repoWorkSpace, 'target/surefire-reports/com.driver.test.VehicleTest.txt')
+        path.resolve(repoWorkSpace, 'target/surefire-reports/com.driver.test.TestCases.txt')
       );
       let junitString = junitReports.toString();
       junitString = junitString.split('\n')[3];
       process.stderr.write(`\n${junitString}`);
-      let testResults = junitString.replace(/[^0-9.]/g,' ').split(' ');
-      testResults = testResults.filter(element => !['.',''].includes(element));
+      let testResult = junitString.replace(/[^0-9.]/g,' ').split(' ');
+      testResult = testResult.filter(element => !['.',''].includes(element));
       
-      process.stderr.write(`\nTotal Test Cases: ${parseInt(testResults[0])}`);
-      process.stderr.write(`\nFailed Test Cases: ${parseInt(testResults[1])}`);
+      process.stdout.write(`\nTotal Test Cases: ${parseInt(testResult[0])}`);
+      process.stdout.write(`\nFailed Test Cases: ${parseInt(testResult[1])}`);
+
+      process.stdout.write(`\nEvaluating score...\n`);
+      
+      const totalTests = parseInt(testResult[0]);
+      const totalPassed = (parseInt(testResult[0]) - parseInt(testResult[1]));
+
+      let testResults = {
+        totalTests,
+        totalPassed,
+      }
+      
+      process.stdout.write(`\n${token}`);
+      process.stdout.write(`\n${testResults}`);
+      process.stdout.write(`\n${assignmentName}`);
+      process.stdout.write(`\n${repoName}`);
+      process.stdout.write(`\n${studentUserName}`);
+
+      const {data: score} = await axios.post(
+        `${ACCIO_API_ENDPOINT}/github/get-score`,
+        {
+          token,
+          testResults,
+          assignmentName,
+          repoName,
+          studentGithubUserName: studentUserName
+        }
+      );
     }
+    
+    if (error instanceof Error) core.setFailed(error.message);
+    process.stderr.write(`\nError: ${(error as Error).message}`);
+    process.exit(1);
   }
 }
-
 run();
